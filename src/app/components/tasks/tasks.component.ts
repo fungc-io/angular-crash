0612061fc076f8087e51a33068cc89c0d811167e
Fung Cheng <lamfung.cheng@gmail.com>
2021-05-10 22:14:02 +0800

    add json-server for mock server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {Task} from '../../Task'
-import {TASKS } from '../../mock-tasks'
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from '../../Task'
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS
-  constructor() { }
+  tasks: Task[] = []
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks
+    })
   }
 
 }
